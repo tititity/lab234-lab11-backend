a9e69f02d2c475a53d7e04bf3ea7bccc26a78b6b
Unknown <spsr5032@gmail.com>
2018-04-27 10:26:47 +0700

    override equals & hasCodes

--- src/main/java/camt/se234/lab11/entity/Student.java
+++ src/main/java/camt/se234/lab11/entity/Student.java
@@ -39,5 +39,26 @@ public class Student {
     public Double getGpa() {
         return gpa;
     }
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (studentId != null ? !studentId.equals(student.studentId) : student.studentId != null) return false;
+        if (name != null ? !name.equals(student.name) : student.name != null) return false;
+        if (surName != null ? !surName.equals(student.surName) : student.surName != null) return false;
+        return gpa != null ? gpa.equals(student.gpa) : student.gpa == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = studentId != null ? studentId.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (surName != null ? surName.hashCode() : 0);
+        result = 31 * result + (gpa != null ? gpa.hashCode() : 0);
+        return result;
+    }
 }
